2f3f5d5da778d5ba7081610291c3d43652955f98
Cxvarisorka <130844113+Cxvarisorka@users.noreply.github.com>
2024-03-26 15:23:20 +0400

    signup

--- src/pages/contactus.jsx
+++ src/pages/contactus.jsx
@@ -74,58 +74,6 @@ const ContactUs = () => {
         }
     ]
 
-
-    const infoContact = [
-        {
-            title: "Address",
-            icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
-            </svg>,
-            text1: "225 8th Ave, New York, NY 199980, USA",
-            text2: "Kharebavas street"
-        },
-        {
-            title: "Contact",
-            icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
-            </svg>,
-            text1: "Phone: +995 555 277 335",
-            text2: "E-mail: lcxvaradzee400@gmail.com"
-        },
-        {
-            title: "Hours of Operation",
-            icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-            </svg>,
-            text1: "Monday - Friday: 10:00 - 20:00",
-            text2: "Sunday & Saturday: 9:30 - 22:00"
-        }
-    ];
-
-    const inputs = [
-        {
-            placeholder: "Enter your name*",
-            type: "text",
-            name: "firstname"
-        },
-        {
-            placeholder: "Enter your lastname*",
-            type: "text",
-            name: "lastname"
-        },
-        {
-            placeholder: "Enter your email*",
-            type: "email",
-            name: "email"
-        },
-        {
-            placeholder: "Enter your subject*",
-            type: "text",
-            name: "subject"
-        }
-    ]
-
     return (
         <>
             <DynamicHero title={"Contact Us"}/>
